d7b5c493a1c073460e37ddfd5f17cdb7d614f7f3
Felipe Bojórquez García <github@fbg.mx>
2018-12-02 13:04:50 -0600

    Remove dummy splash screen button

--- app/src/main/java/com/iteso/trace/ActivitySplashScreen.java
+++ app/src/main/java/com/iteso/trace/ActivitySplashScreen.java
@@ -101,11 +101,6 @@ public class ActivitySplashScreen extends AppCompatActivity {
                 toggle();
             }
         });
-
-        // Upon interacting with UI controls, delay any scheduled hide()
-        // operations to prevent the jarring behavior of controls going away
-        // while interacting with the UI.
-        findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
     }
 
     @Override
