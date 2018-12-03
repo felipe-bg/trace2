027790f51a5bdaca66367691f265988defc4333b
Felipe Bojórquez García <github@fbg.mx>
2018-12-02 20:57:43 -0600

    Add AppBar menu items

--- app/src/main/java/com/iteso/trace/ActivityMain.java
+++ app/src/main/java/com/iteso/trace/ActivityMain.java
@@ -67,7 +67,7 @@ public class ActivityMain extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.menu_activity_channel_title) {
             return true;
         }
 
